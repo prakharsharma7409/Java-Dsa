3eef327743f2dea6268a0d103370019cca8b14ec
Prakhar Sharma <167796535+prakharsharma7409@users.noreply.github.com>
2025-02-10 23:08:44 +0530

    Add files via upload

---
+++ Sorting/InbuiltSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class InbuiltSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+
+        // Sorting array in ascending order
+        Arrays.sort(arr);
+
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+/*import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] arr = {5, 2, 9, 1, 5, 6};
+
+        // Sorting array in descending order
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        // Printing sorted array
+        System.out.println(Arrays.toString(arr));
+    }
+}*/
+
